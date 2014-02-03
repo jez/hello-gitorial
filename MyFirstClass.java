451408b49d57b364c317b9a4905b36ff68f4c76a
Jacob Zimmerman <zimmerman.jake@gmail.com>
2014-02-02 19:16:31 -0500

    Removing files

--- MyFirstClass.java
+++ MyFirstClass.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MyFirstClass {
   public static void main(String[] args) {
     System.out.println("Hello, world!");
-    System.out.println("Do you want to say something to the Java world!")
+    System.out.println("Do you want to say something to the Java world!");
     Scanner scan = new Scanner(System.in);
     String greeting = scan.nextLine();
     System.out.println(">>> You said: " + greeting);
--- README.md
+++
@@ -1,4 +0,0 @@
-hello-gitorial
-==============
-
-Simple tutorial repository where every commit is a new step in the tutorial. www.gitorial.com
