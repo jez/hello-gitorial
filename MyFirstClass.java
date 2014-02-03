38c8bad576a0838103eb7473162433df9ed48af7
Jacob Zimmerman <zimmerman.jake@gmail.com>
2014-02-02 19:11:06 -0500

    Added some java.util.Scanner action

--- MyFirstClass.java
+++ MyFirstClass.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class MyFirstClass {
   public static void main(String[] args) {
     System.out.println("Hello, world!");
+    System.out.println("Do you want to say something to the Java world!")
+    Scanner scan = new Scanner(System.in);
+    String greeting = scan.nextLine();
+    System.out.println(">>> You said: " + greeting);
+    System.out.println("Thanks! It's great to meet you!");
   }
 }
